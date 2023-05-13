41cb45cad31ca01c843f61e65aa929b19bfd74a3
Aamir Hussain <aamirbhat382@gmail.com>
2023-05-13 09:26:33 +0530

    fix allowFullScreen error

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                     <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md overflow-x-hidden ">
                        {/* Map */}
                       
-                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.730835270368!2d74.28560057431014!3d34.40818679885427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e11cf52d09c025%3A0xe4a47bf8cdaecb09!2sGovt%20Degree%20College%20Handwara!5e0!3m2!1sen!2sin!4v1683875103987!5m2!1sen!2sin" width="600" height="450" className="i-map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.730835270368!2d74.28560057431014!3d34.40818679885427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e11cf52d09c025%3A0xe4a47bf8cdaecb09!2sGovt%20Degree%20College%20Handwara!5e0!3m2!1sen!2sin!4v1683875103987!5m2!1sen!2sin" width="600" height="450" className="i-map" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
